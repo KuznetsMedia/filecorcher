30adfbe4e623de5992106508dbc6f1d149bc30cf
Anton Kuznetsov <Kuznetsov.antoha2002@gmail.com>
2024-10-17 10:00:35 +0500

    +Тестирование rename

--- src/filecorcher/filecorcher.py
+++ src/filecorcher/filecorcher.py
@@ -9,3 +9,7 @@ writelog = True
 path = "C:/Users/Антон/filecorcher/test"
 filename = "test.csv"
 csvf.sepcol(path, filename, [2, 3, 4], [0, 1], encoding="windows-1251")
+
+path = "C:/Users/Антон/filecorcher/test/testfolder"
+pattern = ["<counter>3", "_", "<filename>"]
+print(fs.rename(path, pattern, "directory", writelog=writelog))
